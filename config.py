76101279451e00ce63e49972eb9292a67710885f
Amrit Mishra <bkmishra77amrit@gmail.com>
2024-11-12 08:47:48 +0530

    config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1679112664"))
-SUPPORT_GRP = "+cXIPgHSuJnxiNjU1"
-UPDATE_CHNL = "ai_image_junction"
-OWNER_USERNAME = "OfficialDurgesh"
+OWNER_ID = int(getenv("OWNER_ID", "6616914609"))
+SUPPORT_GRP = "mishraXhub"
+UPDATE_CHNL = "MishraXnetwork"
+OWNER_USERNAME = "BRAND_BRAHMAN"
